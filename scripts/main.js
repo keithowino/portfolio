3ee62b04898e72b630f89c30c7295d6efc8ec316
Keith Owino <designsolutions1629@gmail.com>
2023-09-01 18:33:56 +0300

    Modify footer, body and title tags

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,21 @@
 "use strict";
 
-const appName = "app name";
+const appName = "keith owino";
+const appNameCapitalizeAll = appName.toUpperCase();
 
-document.title = appName.toUpperCase();
+const remarkOptions = [
+    "Designer and Frontend Developer.", 
+    "Connect, inspire, develop.", 
+    "Welcome to my humble aboard.", 
+    "Do it in kind, shall receive it in kind."
+];
+const remarkOptionsLastElement = remarkOptions.length - 1;
+function documentTitleStatus(argument1, argument2, argument3){
+    let remarkOptionRange = Math.floor(Math.random() * (argument3 - argument2 + 1)) + argument2;
+    let remark = remarkOptions[remarkOptionRange];
+    document.title = argument1 + " | " + remark;
+};
+documentTitleStatus(appNameCapitalizeAll, 0, remarkOptionsLastElement);
 
 const titleElement = document.querySelector("#js-s-t-s");
 const siteTileRefElement = document.querySelector(".js-site-title-ref");
